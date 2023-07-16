e011f497693d8004a524d876b19fde6c4e04cfcc
nottundeednut <70321260+nottundeednut@users.noreply.github.com>
2023-07-16 08:12:56 +0100

    change font colors in dark background

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
 const h2 = document.querySelector('h2');
 
 const changeColor = () => {
@@ -7,9 +8,23 @@ const changeColor = () => {
   const blue = Math.floor(Math.random() * 255);
 
   const newColor = `rgb(${red}, ${green}, ${blue})`;
-  document.body.style.backgroundColor = newColor;
 
-  h2.innerText = `Color Code: ${newColor}`;
+  if (red <= 100 && green <= 100 && blue <= 100){
+    document.body.style.backgroundColor = newColor;
+    h2.innerText = `Color Code: ${newColor}`;
+    h1.style.color = 'white';
+    h2.style.color = 'white';
+  }
+  else {
+    document.body.style.backgroundColor = newColor;
+    h2.innerText = `Color Code: ${newColor}`;
+    h1.style.color = 'black';
+    h2.style.color = 'black';
+  }
+
+ 
+  
+
 };
 
 btn.addEventListener('click', changeColor);
